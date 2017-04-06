ab3dd014e2b64b8f4f85f89fcf97c7f9ca9f3313
Máté <dontirikk@gmail.com>
2017-04-06 14:17:05 +0200

    add 'Add new task' functionality

--- src/Task.java
+++ src/Task.java
@@ -1,10 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class Task {
-  private String state;
-  private String description;
+  String state;
+  String description;
 
   public Task(String description) {
     this.state = "done";
     this.description = description;
-
+    try {
+      Files.write(Paths.get("src/tasks.csv"), (description + "\n").getBytes(), StandardOpenOption.APPEND);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
