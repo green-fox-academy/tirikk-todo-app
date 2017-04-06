3cf46af06374528fadb4bb196d0297cd50e8afb7
Máté <dontirikk@gmail.com>
2017-04-06 12:26:33 +0200

    add 'Print usage' functionality

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
   public static void main(String[] args) {
-    
+    if (args.length == 0) {
+      printUsage();
+    }
+  }
+
+  public static void printUsage() {
+    System.out.println("Java Todo application\n" +
+            "=====================\n" +
+            "\n" +
+            "Command line arguments:\n" +
+            " -l   Lists all the tasks\n" +
+            " -a   Adds a new task\n" +
+            " -r   Removes an task\n" +
+            " -c   Completes an task");
   }
 }
